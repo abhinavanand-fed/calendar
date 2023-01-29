e355973af0cc055c5c2d8f1c5ad94d61e957fcfb
abhinavanand-fed <abhinavanand080@gmail.com>
2023-01-29 21:37:40 +0530

    calendar.js.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./calendar.css";
+import "./App.css";
 
-function App() {
+function Calendar() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const handlePrevMonth = () => {
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate()));
+  };
+
+  const handleNextMonth = () => {
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate()));
+  };
+
+  const daysInMonth = (month, year) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+
+  const dates = [];
+  for (let i = 1; i <= daysInMonth(currentDate.getMonth(), currentDate.getFullYear()); i++) {
+    dates.push(i);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="calendar-title">Calendar</h1>
+    <div className="calendar">
+      <div className="calendar-header">
+        <button className="calendar-arrow" onClick={handlePrevMonth}>{"<"}</button>
+        <h1 className="calendar-month-year">
+          {monthNames[currentDate.getMonth()]} {currentDate.getFullYear()}
+        </h1>
+        <button className="calendar-arrow" onClick={handleNextMonth}>{">"}</button>
+      </div>
+      <table className="calendar-table">
+        <thead>
+          <tr>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({ length: Math.ceil(dates.length / 7) }, (_, weekIndex) => (
+            <tr key={weekIndex}>
+              {Array.from({ length: 7 }, (_, dayIndex) => {
+                const dateIndex = weekIndex * 7 + dayIndex;
+                const date = dateIndex - firstDayOfMonth + 1;
+                return (
+                  <td key={dayIndex}>
+                    {date > 0 && date <= dates.length ? date : ""}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
     </div>
   );
 }
 
-export default App;
+export default Calendar;
